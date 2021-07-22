dd3fa6576a871026e62a06ff40d8e3eadb458247
matveyprupas <matveyprupas@gmail.com>
2021-07-22 09:45:31 +0300

    fix: add function for burger-menu

--- english-for-kids/scripts/header.js
+++ english-for-kids/scripts/header.js
@@ -1,11 +1,18 @@
 let switchBtn = document.querySelector("div.header__switch");
 switchBtn.addEventListener("click", switchMode);
+let burger = document.querySelector("div.header__burger");
+burger.addEventListener("click", openMenu);
 
-function switchMode (e) {
-    let text = switchBtn.querySelector("span");
-    let round = switchBtn.querySelector("div.header__switch-round");
-
+function switchMode () {
     switchBtn.classList.toggle("checked");
-    
-    console.log(switchBtn, text, round);
+}
+
+function openMenu () {
+    burger.classList.toggle("header__menu-opened");
+    let lineArray = burger.querySelectorAll("line");
+    // lineArray[1].style.opacity = 0;
+    // lineArray[1].setAttribute("x", "40");
+
+    // lineArray[0].setAttribute();
+    console.log(lineArray);
 }
