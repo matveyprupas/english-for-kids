55cf4e84ee4b8694dd525d382817cfc80180c893
matveyprupas <matveyprupas@gmail.com>
2021-07-22 17:42:03 +0300

    fix: header done

--- english-for-kids/scripts/header.js
+++ english-for-kids/scripts/header.js
@@ -10,9 +10,17 @@ function switchMode () {
 function openMenu () {
     burger.classList.toggle("header__menu-opened");
     let lineArray = burger.querySelectorAll("line");
-    // lineArray[1].style.opacity = 0;
-    // lineArray[1].setAttribute("x", "40");
+
+    if(burger.classList.length > 1) {
+        // lineArray[0].setAttribute("y2", "50");
+        // lineArray[2].setAttribute("y2", "00");
+    } else {
+        // lineArray[0].setAttribute("y2", "0");
+        // lineArray[2].setAttribute("y2", "50");
+    }
+    // lineArray[0].style.opacity = 0;
+    
 
     // lineArray[0].setAttribute();
-    console.log(lineArray);
+    // console.log(lineArray);
 }
